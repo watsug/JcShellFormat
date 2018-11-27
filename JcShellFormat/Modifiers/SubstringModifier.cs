0bc840e8258150bb6befd54f68c1aa944eb480bb
Adam Augustyn <adam@augustyn.net>
2018-11-27 22:04:58 +0100

    substring implemented

--- JcShellFormat/Modifiers/SubstringModifier.cs
+++ JcShellFormat/Modifiers/SubstringModifier.cs
@@ -1,4 +1,6 @@
-﻿namespace Watsug.JcShellFormat.Modifiers
+﻿using System;
+
+namespace Watsug.JcShellFormat.Modifiers
 {
     public class SubstringModifier : BaseModifier
     {
@@ -9,7 +11,29 @@
 
         public override string Transform(string text)
         {
-            throw new System.NotImplementedException();
+            var p = FormatParameters.Split(',');
+            int off = int.Parse(p[0]);
+            if (off < 0 || off > text.Length)
+            {
+                throw new ArgumentOutOfRangeException($"Out of range offset: {off} (0 - {text.Length})!");
+            }
+            int len = -1;
+            if (p.Length > 1)
+            {
+                len = int.Parse(p[1]);
+            }
+
+            if (len < 0)
+            {
+                len = text.Length - off;
+            }
+
+            if (off + len > text.Length)
+            {
+                throw new ArgumentOutOfRangeException($"Out of range length: {len} (0 - {text.Length - off})!");
+            }
+
+            return text.Substring(off, len);
         }
     }
 }
--- JcShellFormatTests/VariableModifiersTests.cs
+++ JcShellFormatTests/VariableModifiersTests.cs
@@ -1,4 +1,6 @@
-﻿namespace Watsug.JcShellFormatTests
+﻿using System;
+
+namespace Watsug.JcShellFormatTests
 {
     using NUnit.Framework;
     using JcShellFormat;
@@ -44,10 +46,45 @@
         }
 
         [Test]
-        public void PositiveSimpeLowercaseTest()
+        public void PositiveLowercaseTest()
         {
             var jcs = new JcShellFormat("${x1;lc}", new Dictionary<string, string> { { "x1", "WaTsUg" } });
             Assert.AreEqual("watsug", jcs.Evaluate());
+        }
+
+        [Test]
+        public void PositiveSubstringTest()
+        {
+            var jcs = new JcShellFormat("${text;s5,7}", new Dictionary<string, string> { { "text", "this is my test" } });
+            Assert.AreEqual("is my t", jcs.Evaluate());
+        }
+
+        [Test]
+        public void PositiveSubstringAtTheEndTest()
+        {
+            var jcs = new JcShellFormat("${text;s15}", new Dictionary<string, string> { { "text", "this is my test" } });
+            Assert.AreEqual("", jcs.Evaluate());
+        }
+
+        [Test]
+        public void PositiveSubstringAtTheEndNegativeLengthTest()
+        {
+            var jcs = new JcShellFormat("${text;s15,-1}", new Dictionary<string, string> { { "text", "this is my test" } });
+            Assert.AreEqual("", jcs.Evaluate());
+        }
+
+        [Test]
+        public void NegativeSubstringOneCharAfterTheEndTest()
+        {
+            var jcs = new JcShellFormat("${text;s16}", new Dictionary<string, string> { { "text", "this is my test" } });
+            Assert.Throws<ArgumentOutOfRangeException>(() => jcs.Evaluate());
+        }
+
+        [Test]
+        public void NegativeSubstringAtTheEndWithOffsetTest()
+        {
+            var jcs = new JcShellFormat("${text;s15,1}", new Dictionary<string, string> { { "text", "this is my test" } });
+            Assert.Throws<ArgumentOutOfRangeException>(() => jcs.Evaluate());
         }
     }
 }
